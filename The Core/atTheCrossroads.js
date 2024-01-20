6bd7dee5a353edf425d9b6bf7e06054bdee0eaf2
Rafael Santos <rafasantosbzr@icloud.com>
2024-01-20 11:08:57 -0300

    chore: knapsack light challenge finished

--- The Core/atTheCrossroads.js
+++ The Core/atTheCrossroads.js
@@ -5,3 +5,31 @@ function solution(experience, threshold, reward) {
 }
 
 //knapsack light
+function solution(value1, weight1, value2, weight2, maxW) {
+    const bag = [weight1, weight2];
+    const value = [value1, value2];
+    let maxV = 0;
+    
+    if (value2 > value1) {
+        if (bag[1] <= maxW) {
+        maxV += value[1];
+        maxW -= bag[1];
+        }
+    
+        if (bag[0] <= maxW) {
+        maxV += value[0];
+        maxW -= bag[0];
+        }
+    } else {
+        if (bag[0] <= maxW) {
+        maxV += value[0];
+        maxW -= bag[0];
+        }
+    
+        if (bag[1] <= maxW) {
+        maxV += value[1];
+        maxW -= bag[1];
+        }
+    }   
+    return maxV;
+}
